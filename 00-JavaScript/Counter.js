a0edfb8447a9ac326a31915ae04484a082e9091b
Eliott Vincent <eliott.20100@gmail.com>
2017-10-25 15:23:00 +0200

    00 sample

---
+++ 00-JavaScript/Counter.js
@@ -0,0 +1,28 @@
+﻿// Définition de la classe Counter
+// cette classe est un compteur
+
+
+class Counter
+{
+    /** constructeur */
+    constructor(initval=0)
+    {
+        this.m_Value = initval;
+        console.debug("initialisation à "+initval);
+    }
+
+
+    /** getter */
+    getCount()
+    {
+        return this.m_Value;
+    }
+
+
+    /** incrémentation */
+    increment()
+    {
+        this.m_Value++;
+        console.debug("incrementation");
+    }
+}
---
+++ 00-JavaScript/CounterStep.js
@@ -0,0 +1,21 @@
+﻿// Définition de la classe CounterStep
+// cette classe est un compteur de N en N
+
+
+class CounterStep extends Counter
+{
+    /** constructeur */
+    constructor(step, initval=0)
+    {
+        super(initval);
+        this.m_Step = step;
+    }
+
+
+    /** incrémentation */
+    increment()
+    {
+        this.m_Value += this.m_Step;
+        console.debug("incrementation");
+    }
+}
