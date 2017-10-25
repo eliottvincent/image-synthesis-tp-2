284554608935a2960841028d77adaaaf7282892e
Eliott Vincent <eliott.20100@gmail.com>
2017-10-25 15:26:02 +0200

    09 cube : new example with cube

---
+++ 09-Dessin3D-cube/Cube.js
@@ -0,0 +1,173 @@
+﻿// Définition de la classe Cube
+
+
+class Cube
+{
+    /** constructeur */
+    constructor()
+    {
+        /** shader */
+
+            // version WebGL2
+
+        let srcVertexShaderV2 = dedent
+            `#version 300 es
+            uniform mat4 matrix;
+            in vec3 glVertex;
+            in vec3 glColor;
+            out vec3 frgColor;
+            void main()
+            {
+                gl_Position = matrix * vec4(glVertex, 1.0);
+                frgColor = glColor;
+            }`;
+
+        let srcFragmentShaderV2 = dedent
+            `#version 300 es
+            precision mediump float;
+            const vec3 fog_color = vec3(0.9, 0.9, 0.9);
+            in vec3 frgColor;
+            out vec4 glFragColor;
+            void main()
+            {
+                // distance entre l'écran et le fragment
+                float dist = gl_FragCoord.z / gl_FragCoord.w;
+                glFragColor = vec4(dist - 9.0, 9.0 - dist, 0, 1);return;
+                // taux de brouillard en fonction de la distance
+                float fog = clamp((dist-6.0)/6.0, 0.0, 1.0);
+                //glFragColor = vec4(fog, fog, fog, 1);return;
+                // mélange couleur et brouillard
+                //glFragColor = vec4(mix(frgColor, fog_color, fog), 1.0);
+
+                // pour la mise au point de la perspective (near et far)
+                glFragColor = vec4(frgColor, 1.0);
+            }`;
+
+        // compiler le shader de dessin
+        this.m_ShaderId = Utils.makeShaderProgram(srcVertexShaderV2, srcFragmentShaderV2, "Cube");
+        console.debug("Source du vertex shader :\n"+srcVertexShaderV2);
+        console.debug("Source du fragment shader :\n"+srcFragmentShaderV2);
+
+        // déterminer où sont les variables attribute et uniform
+        this.m_MatrixLoc = gl.getUniformLocation(this.m_ShaderId, "matrix");
+        this.m_VertexLoc = gl.getAttribLocation(this.m_ShaderId, "glVertex");
+        this.m_ColorLoc = gl.getAttribLocation(this.m_ShaderId, "glColor");
+
+
+        /** VBOs */
+            // créer et remplir le buffer des coordonnées
+        const b = 0.25;
+        let vertices = [
+            +2*b, 0, +2*b,     // P0
+            +2*b, 0, -2*b,     // P1
+            -2*b, 0, -2*b,     // P2
+            -2*b, 0, +2*b,     // P3
+
+            +2*b, +4*b, +2*b,     // P4
+            +2*b, +4*b, -2*b,     // P5
+            -2*b, +4*b, -2*b,     // P6
+            -2*b, +4*b, +2*b,     // P7
+        ];
+        this.m_VertexBufferId = Utils.makeFloatVBO(vertices, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+
+        // créer et remplir le buffer des couleurs
+        let colors = [
+            0.0, 0.5, 1.0,  // P0 bleu
+            1.0, 0.5, 0.0,  // P1 orange
+            0.0, 1.0, 0.0,  // P2 vert
+            0.7, 0.0, 0.7,  // P3 violet
+
+            0.0, 0.5, 1.0,  // P4 bleu
+            1.0, 0.5, 0.0,  // P5 orange
+            0.0, 1.0, 0.0,  // P6 vert
+            0.7, 0.0, 0.7,  // P7 violet
+
+        ];
+        this.m_ColorBufferId = Utils.makeFloatVBO(colors, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+
+        // créer et remplir le buffer des indices
+        // attention, l'ordre est inversé car il n'y a pas de projection perspective, donc
+        // on est en repère main gauche
+        let indexlist = [
+            0, 1, 4,
+            5, 4, 1,
+            4, 5, 7,
+            6, 7, 5,
+            6, 2, 7,
+            3, 7, 2,
+            3, 2, 0,
+            1, 0, 2,
+
+            3, 0, 7,
+            4, 7, 0,
+            2, 6, 1,
+            6, 5, 1,
+        ];
+        this.TRIANGLE_COUNT = indexlist.length / 3;
+        this.m_IndexBufferId = Utils.makeShortVBO(indexlist, gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW);
+
+        // matrices de transformation intermédiaires (on pourrait économiser l'une d'elles)
+        this.m_MatVM = mat4.create();       // V * M
+        this.m_MatPVM = mat4.create();      // P * V * M
+    }
+
+
+    /**
+     * dessiner l'objet
+     * @param matP : matrice de projection perpective
+     * @param matV : matrice de transformation de l'objet par rapport à la caméra
+     */
+    onDraw(matP, matV)
+    {
+        // rajouter une translation pour décaler l'objet d'arrière en avant
+        //mat4.translate(this.m_MatVM, matV, vec3.fromValues(0, 0, 3.0*Math.sin(Utils.Time*0.5)));
+        mat4.translate(this.m_MatVM, matV, vec3.fromValues(0, 0, 3.0*Math.sin(100*0.5)));
+
+        // rajouter des rotations pour faire basculer l'objet dans tous les sens
+        mat4.rotate(this.m_MatVM, this.m_MatVM, Utils.radians(45.0 * Utils.Time), vec3.fromValues(0,1,0));
+        mat4.rotate(this.m_MatVM, this.m_MatVM, Utils.radians(67.0 * Utils.Time), vec3.fromValues(0.75,0,0.5));
+
+        // activer le shader
+        gl.useProgram(this.m_ShaderId);
+
+        // fournir la matrice P * V * M au shader
+        mat4.mul(this.m_MatPVM, matP, this.m_MatVM);
+        mat4.glUniformMatrix(this.m_MatrixLoc, this.m_MatPVM);
+
+        // activer et lier le buffer contenant les coordonnées
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.m_VertexBufferId);
+        gl.enableVertexAttribArray(this.m_VertexLoc);
+        gl.vertexAttribPointer(this.m_VertexLoc, Utils.VEC3, gl.FLOAT, gl.FALSE, 0, 0);
+
+        // activer et lier le buffer contenant les couleurs
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.m_ColorBufferId);
+        gl.enableVertexAttribArray(this.m_ColorLoc);
+        gl.vertexAttribPointer(this.m_ColorLoc, Utils.VEC3, gl.FLOAT, gl.FALSE, 0, 0);
+
+        // activer et lier le buffer contenant les indices
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.m_IndexBufferId);
+
+        // dessiner les triangles
+        gl.drawElements(gl.TRIANGLES, this.TRIANGLE_COUNT * 3, gl.UNSIGNED_SHORT, 0);
+
+        // désactiver les buffers
+        gl.disableVertexAttribArray(this.m_VertexLoc);
+        gl.disableVertexAttribArray(this.m_ColorLoc);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+        // désactiver le shader
+        gl.useProgram(null);
+    }
+
+
+    /** destructeur */
+    destroy()
+    {
+        // supprimer le shader et les VBOs
+        Utils.deleteShaderProgram(this.m_ShaderId);
+        Utils.deleteVBO(this.m_VertexBufferId);
+        Utils.deleteVBO(this.m_ColorBufferId);
+        Utils.deleteVBO(this.m_IndexBufferId);
+    }
+}
