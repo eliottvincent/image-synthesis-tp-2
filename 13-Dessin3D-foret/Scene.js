365e74c8606b1dbb7491f6f56947c47585a30024
Eliott Vincent <eliott.20100@gmail.com>
2017-10-28 12:15:10 +0200

    13 foret : added loop for pyramids postioning

--- 13-Dessin3D-foret/Scene.js
+++ 13-Dessin3D-foret/Scene.js
@@ -10,7 +10,7 @@ class Scene
     constructor()
     {
         // créer les objets à dessiner
-        this.m_GrilleXZ = new GrilleXZ();
+        this.m_GrilleXZ = new GrilleXZ(50, 50);
         this.m_Pyramide = new Pyramide();
 
         // couleur du fond : gris foncé
@@ -24,9 +24,11 @@ class Scene
         gl.cullFace(gl.BACK);
 
         // créer les matrices utilisées
-        this.m_MatViewModel = mat4.create();
         this.m_MatView = mat4.create();
+        this.m_MatViewModel = mat4.create();
         this.m_MatProjection = mat4.create();
+
+        this.m_numberOfPyramids = 10;
     }
 
 
@@ -41,7 +43,7 @@ class Scene
         gl.viewport(0, 0, width, height);
 
         // matrice de projection
-        mat4.perspective(this.m_MatProjection, Utils.radians(20.0), width / height, 0.1, 25.0);
+        mat4.perspective(this.m_MatProjection, Utils.radians(30.0), width/height, 0.1, 50.0);
     }
 
 
@@ -51,22 +53,26 @@ class Scene
     onDrawFrame()
     {
         // effacer l'écran
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        // positionner la caméra en (0,0,15), c'est à dire la scène est décalée en z=-9
+        // positionner la caméra en (0,0,10), c'est à dire la scène est décalée en z=-10
         mat4.identity(this.m_MatView);
-        mat4.translate(this.m_MatView, this.m_MatView, vec3.fromValues(0, 0, -15));
-        mat4.rotateX(this.m_MatView, this.m_MatView, Utils.radians(20.0));
-        mat4.rotateY(this.m_MatView, this.m_MatView, Utils.radians(30.0));
-
-        mat4.identity(this.m_MatViewModel);
-        mat4.translate(this.m_MatViewModel, this.m_MatViewModel, vec3.fromValues(0, 0, -15));
-        mat4.rotateX(this.m_MatViewModel, this.m_MatViewModel, Utils.radians(20.0));
-        mat4.rotateY(this.m_MatViewModel, this.m_MatViewModel , Utils.radians(30.0) * Utils.Time);
+        mat4.translate(this.m_MatView, this.m_MatView, vec3.fromValues(0, 0, -25));
+        mat4.rotateX(this.m_MatView, this.m_MatView, Utils.radians(40.0));
+        //mat4.rotateY(this.m_MatView, this.m_MatView, Utils.radians(30.0)*Utils.Time);
 
         // dessiner la grille
         this.m_GrilleXZ.onDraw(this.m_MatProjection, this.m_MatView);
-        this.m_Pyramide.onDraw(this.m_MatProjection, this.m_MatViewModel);
+
+        // dessiner la foret
+        for (let i = - this.m_numberOfPyramids / 2; i < this.m_numberOfPyramids / 2; i++) {
+            for (let j = - this.m_numberOfPyramids / 2; j < this.m_numberOfPyramids / 2; j++) {
+
+                mat4.translate(this.m_MatViewModel, this.m_MatView, vec3.fromValues(i, 0, j));
+                mat4.scale(this.m_MatViewModel, this.m_MatViewModel, vec3.fromValues(0.3, 0.5, 0.6));
+                this.m_Pyramide.onDraw(this.m_MatProjection, this.m_MatViewModel);
+            }
+        }
     }
 
 
